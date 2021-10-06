756ce3990f0710862ebd190266ce67188f982c64
Luis <admin@luiscaldas.com>
2021-10-06 17:59:14 +0100

    updated the run script

--- run.sh
+++ run.sh
@@ -1,15 +1,48 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-aircraft="PC-9A"
+# defaults
+aircraft="ec135p2"
 airport="EICK"
 callsign="EIRW"
 
+# function that finds the folder in which the script executing it is located
+function get_folder() {
+
+    # get the folder in which the script is located
+    SOURCE="${BASH_SOURCE[0]}"
+
+    # resolve $SOURCE until the file is no longer a symlink
+    while [ -h "$SOURCE" ]; do
+
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+      SOURCE="$(readlink "$SOURCE")"
+
+      # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+
+    done
+
+    # the final assignment of the directory
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    # return the directory
+    echo "$DIR"
+}
+
+# extract current folder
+current_folder="$(get_folder)"
+
+# auto select aircraft if none given, also print all available
 if [ -n "$1" ]; then
 	aircraft="$1"
+else
+	fgfs --fg-aircraft="$current_folder" --show-aircrafts
 fi
 
+# run the game command
 pp_jimenezmlaa_color=32 fgfs \
-	--fg-aircraft=. \
+	--fg-aircraft="$current_folder" \
 	--aircraft="$aircraft" \
 	--airport="$airport" \
 	--units-meters \
