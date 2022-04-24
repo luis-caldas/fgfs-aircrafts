8c1a4c022ba7ff76d7c0f422e799c69e1e74f3a7
Luis <admin@luiscaldas.com>
2022-04-24 19:58:03 +0100

    updated run script with a few more options and variables

--- run.sh
+++ run.sh
@@ -40,18 +40,38 @@ else
 	fgfs --fg-aircraft="$current_folder" --show-aircrafts
 fi
 
+# auto select callsign if none given
+if [ -n "$2" ]; then
+	callsign_out="$2"
+else
+	callsign_out="${callsign}"
+	echo "Callsign is ${callsign}"
+fi
+
+# auto select airport if none given
+if [ -n "$3" ]; then
+	airport_out="$3"
+else
+	airport_out="${airport}"
+	echo "Airport is ${airport}"
+fi
+
+
 # run the game command
-pp_jimenezmlaa_color=4 FG_HOME="${current_folder}/home" fgfs \
+pp_jimenezmlaa_color=8 \
+OSG_SPLIT_STEREO_AUTO_ADJUST_ASPECT_RATIO=ON \
+FG_HOME="${current_folder}/home" \
+fgfs \
 	--fg-aircraft="${current_folder}/aircrafts" \
 	--aircraft="$aircraft" \
-	--airport="$airport" \
+	--airport="$airport_out" \
 	--units-meters \
 	--time-match-local \
 	--com1="123.4" \
 	--com2="121.5" \
 	--enable-real-weather-fetch \
-	--callsign="$callsign" \
-	--addon="${current_folder}/headtracker" \
+	--callsign="$callsign_out" \
+	--addon="${current_folder}/addons/Headtracker" \
 	--generic=socket,in,25,127.0.0.1,5542,udp,opentrack \
 	--multiplay=out,20,mpserver01.flightgear.org,5000 \
 	--enable-terrasync
